077f83fc72a6af5a82f3845c14494f9f31193d8d
Conor Walsh <cw.conorwalsh@gmail.com>
2015-09-22 13:22:40 +0100

    Added Step Method

---
+++ src/test/agile/labs/unit/TestCounter.java
@@ -0,0 +1,14 @@
+package agile.labs.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCounter {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
